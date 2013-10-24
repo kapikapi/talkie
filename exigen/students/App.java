83fe0d661edd7860083224cb19364427bb0053e3
Kapitonova Elisabeth <hellokapikapi@gmail.com>
2013-10-24 18:39:16 +0400

    пщпщ

--- exigen/students/App.java
+++ exigen/students/App.java
@@ -19,7 +19,7 @@ public class App {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("Окей. До свидания!");
+        System.out.println("Окей. Пока!");
     }
 
     private static void talk(ReplyGenerator replyGenerator) throws IOException {
